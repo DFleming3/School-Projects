6beb8e8e300da05b764337117719b7578f20d32b
Drew Fleming <94998169+dfleming3@users.noreply.github.com>
2021-11-29 01:26:50 -0800

    Create Letter Project

---
+++ Freshman Year/Letter/README.md
@@ -0,0 +1,2 @@
+# Letter Program
+This program creates a sample letter and includes a tongue-in-cheek joke regarding programming terms and their usage.
---
+++ Freshman Year/Letter/src/letter/Letter.java
@@ -0,0 +1,20 @@
+package letter;
+
+public class Letter {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Dear \"Doubleslash\" magazine,");
+        System.out.println("\tYour publication confuses me. Is it a");
+        System.out.println("\\\\ slash or a // slash that I should use?");
+        System.out.println("\nSincerely,");
+        System.out.println("Susan \"Suzy\" Smith");
+        
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"? Or between a \" and a \\\"?");
+        System.out.println("One is what we see when we're typing our program");
+        System.out.println("The other is what appears on the \"console.\"");
+        
+    }
+    
+}
